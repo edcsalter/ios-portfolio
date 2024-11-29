6c755a80cc6f9cf98ca51826e0a148142294ec68
Ed Salter <edcsalter@gmail.com>
2024-11-29 04:47:51 -0500

    Added Vercel SpeedInsights

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 interface LayoutProps {
   children: React.ReactNode;
